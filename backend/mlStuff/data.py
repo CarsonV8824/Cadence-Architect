e573614f22152ce7a290326c6d1717d033c8f871
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-05-21 13:15:42 -0500

    done

--- backend/mlStuff/data.py
+++ backend/mlStuff/data.py
@@ -5,6 +5,7 @@ import pandas as pd
 import markovify
 import pickle
 import random
+from chordparser import Parser
 
 def read_data():
     script_dir = os.path.dirname(os.path.abspath(__file__))
@@ -15,23 +16,41 @@ def read_data():
             for line in file:
                 yield line.decode('utf-8') + "." # period is so the markov libary can understand what is going on
 
-
 def make_sentences():
     new_list = []
     count_line = 0
     for line in read_data():
         new_list.append(line)
         count_line += 1
-        if count_line > 1000:
+        if count_line > 5_000:
             break
+        print(line)
     return " ".join(new_list)
 
-def make_markov_model():
-    random.seed() 
+def make_model():
     the_text = make_sentences()
     model = markovify.Text(the_text)
-    chords = model.make_sentence(test_output=False, tries=1000)
-    print(chords)
+    with open("model.pth", "wb") as file:
+        pickle.dump(model, file)
 
+def make_markov_sentences(chord_length:int=8) -> str:
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    model_path = os.path.join(script_dir, 'model.pth')
+    random.seed() 
+    with open(model_path, "rb") as file:
+        model:markovify.Text = pickle.load(file)
+    print("model loaded")
+    chords = None
+    while chords == None:
+        chords = model.make_sentence(test_output=False, tries=1000)
+    parsed_progressions = chords.split(" . ")
+    choosen = random.choice(parsed_progressions).split()
+    choosen = choosen[:chord_length]
+    try:
+        choosen.remove(".")
+        return "-".join(choosen)
+    except ValueError:
+        return "-".join(choosen)
+    
 if __name__ == "__main__":
-    make_markov_model()
+    print(make_markov_sentences())
