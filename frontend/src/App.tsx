51b59e9ece168a58521a50ae585d430df1a610d6
CarsonV <166647418+CarsonV8824@users.noreply.github.com>
2026-05-23 23:30:39 -0500

    changed to hash router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import NavBar from "./componets/navbar"
 import Home from "./pages/home"
 import Generate from "./pages/generate"
